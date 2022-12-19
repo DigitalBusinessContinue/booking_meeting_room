74b4dd8616d55bdd0f70acf58a035ebec8225ffb
DESKTOP-KPFV3GA\admin <biwpaweena55@gmail.com>
2022-12-19 22:39:27 +0700

    date

--- app/src/main/java/com/example/myk/HomeActivity.kt
+++ app/src/main/java/com/example/myk/HomeActivity.kt
@@ -1,49 +1,28 @@
 package com.example.myk
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.ImageButton
 import android.widget.Toast
 import com.example.myk.Adapter.MyAdapter
+import java.util.Calendar
 
 class HomeActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val picker = findViewById<DatePicker>(R.id.datePicker)
+        val today = Calendar.getInstance()
+        picker.init(today.get(Calendar.YEAR),today.get(Calendar.MONTH),today.get(Calendar.DAY_OF_MONTH)){
+            view, year, moth, day ->
+            val  month = moth+1
 
-
-        val secondActbutton = findViewById<Button>(R.id.button3)
-        secondActbutton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-        val secondActbutton1 = findViewById<Button>(R.id.button5)
-        secondActbutton1.setOnClickListener {
-            val intent = Intent(this, Data1::class.java)
-            startActivity(intent)
-        }
-        val secondActbutton2 = findViewById<Button>(R.id.button6)
-        secondActbutton2.setOnClickListener {
-            val intent = Intent(this, MyAdapter::class.java)
-            startActivity(intent)
-        }
-        val secondActbutton3 = findViewById<Button>(R.id.button7)
-        secondActbutton3.setOnClickListener {
-            val intent = Intent(this, Data3::class.java)
-            startActivity(intent)
-        }
-        val secondActbutton4 = findViewById<Button>(R.id.button8)
-        secondActbutton4.setOnClickListener {
-            val intent = Intent(this, Data4::class.java)
-            startActivity(intent)
-        }
-        val secondActbutton5 = findViewById<Button>(R.id.button9)
-        secondActbutton5.setOnClickListener {
-            val intent = Intent(this, Data5::class.java)
-            startActivity(intent)
         }
     }
 }
